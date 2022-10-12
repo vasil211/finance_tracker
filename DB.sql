3c86338b7d8d2cadf21818b4e7123f1a0e7b5dd9
vasil211 <vasil.vazonov@abv.bg>
2022-10-12 20:59:37 +0300

    Updated DB and action map

--- DB.sql
+++ DB.sql
@@ -2,33 +2,32 @@ drop database if exists finance_tracker;
 create database finance_tracker;
 use finance_tracker;
 
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(25) UNIQUE NOT NULL,
-    password VARCHAR(40) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     email VARCHAR(45) UNIQUE NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     last_login DATETIME
 );
-CREATE TABLE budgets (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    amount DOUBLE not null,
-    from_date DATETIME not null,
-    to_date DATETIME not null
-);
 
+CREATE TABLE currency (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    abriviation VARCHAR(5) NOT NULL,
+    full_name VARCHAR(25) NOT NULL
+);
 CREATE TABLE accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(25) NOT NULL,
-    currency VARCHAR(10) NOT NULL,
+    currency_id int NOT NULL,
     user_id INT NOT NULL,
-    budget_id INT,
     balance DOUBLE,
     FOREIGN KEY (user_id)
         REFERENCES users (id),
-    FOREIGN KEY (budget_id)
-        REFERENCES budgets (id)
+    FOREIGN KEY (currency_id)
+        REFERENCES currency (id)
 );
 
 CREATE TABLE icons (
@@ -38,12 +37,23 @@ CREATE TABLE icons (
 
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(25),
+    name VARCHAR(25) NOT NULL,
     icon_id INT NOT NULL,
     FOREIGN KEY (icon_id)
         REFERENCES icons (id)
 );
-
+CREATE TABLE budgets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    amount DOUBLE NOT NULL,
+    user_id INT NOT NULL,
+    category_id INT NOT NULL,
+    from_date DATETIME NOT NULL,
+    to_date DATETIME NOT NULL,
+    FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    FOREIGN KEY (category_id)
+        REFERENCES categories (id)
+);
 CREATE TABLE transfers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     amount DOUBLE NOT NULL,
@@ -63,7 +73,7 @@ CREATE TABLE transactions (
     account_id INT NOT NULL,
     amount DOUBLE NOT NULL,
     category_id INT NOT NULL,
-    date_of_transactions DATETIME,
+    created_at DATETIME NOT NULL,
     description VARCHAR(255),
     FOREIGN KEY (category_id)
         REFERENCES categories (id),
@@ -74,8 +84,7 @@ CREATE TABLE transactions (
 CREATE TABLE scheduled_payments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     amount DOUBLE NOT NULL,
-    status BOOLEAN DEFAULT FALSE,
-    date_of_transactions DATETIME NOT NULL,
+    due_date DATETIME NOT NULL,
     title VARCHAR(25) NOT NULL,
     category_id INT NOT NULL,
     account_id INT NOT NULL,
