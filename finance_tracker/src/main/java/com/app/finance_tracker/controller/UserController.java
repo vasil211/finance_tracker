fada19b1e69d3e1c683a4aacc8142fd55951ee6d
unknown <morskijk@gmail.com>
2022-10-14 14:04:11 +0300

    no idea

--- finance_tracker/src/main/java/com/app/finance_tracker/controller/UserController.java
+++ finance_tracker/src/main/java/com/app/finance_tracker/controller/UserController.java
@@ -3,6 +3,8 @@ package com.app.finance_tracker.controller;
 import com.app.finance_tracker.model.entities.User;
 import com.app.finance_tracker.model.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
