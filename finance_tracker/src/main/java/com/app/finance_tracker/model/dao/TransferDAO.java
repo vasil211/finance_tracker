eac58c2d81746d6b27d4c950f369bcefb3435580
unknown <morskijk@gmail.com>
2022-10-27 23:28:10 +0300

    fixed little bug from conflict

--- finance_tracker/src/main/java/com/app/finance_tracker/model/dao/TransferDAO.java
+++ finance_tracker/src/main/java/com/app/finance_tracker/model/dao/TransferDAO.java
@@ -54,12 +54,7 @@ public class TransferDAO {
     private void initialSelectWithFromYourAccount(List<Long> toAccountsIds, List<Long> ownAccountsIds,
                                                   LocalDate fromDate, LocalDate toDate, double fromAmount,
                                                   double toAmount, List<Long> currencies, StringBuilder sb) {
-<<<<<<< HEAD
         sb.append("SELECT id,amount,currency_id,from_user_account_id,to_user_account_id,date_of_transfer,description FROM transfers WHERE");
-=======
-        sb.append("SELECT id,amount,currency_id,from_user_account_id,to_user_account_id,date_of_transfer, description " +
-                "FROM transfers WHERE");
->>>>>>> refs/remotes/origin/main
         sb.append(" from_user_account_id");
         if (ownAccountsIds.size() == 1) {
             sb.append(" = ").append(ownAccountsIds.get(0));
@@ -80,12 +75,8 @@ public class TransferDAO {
     private void initialSelectWithToYourAccount(List<Long> fromAccountsIds, List<Long> ownAccountsIds,
                                                 LocalDate fromDate, LocalDate toDate, double fromAmount,
                                                 double toAmount, List<Long> currencies, StringBuilder sb) {
-<<<<<<< HEAD
-        sb.append("SELECT id,amount,currency_id,from_user_account_id,to_user_account_id,date_of_transfer,description FROM transfers WHERE");
-=======
         sb.append("SELECT id,amount,currency_id,from_user_account_id,to_user_account_id,date_of_transfer, description" +
                 " FROM transfers WHERE");
->>>>>>> refs/remotes/origin/main
         sb.append(" to_user_account_id");
         if (ownAccountsIds.size() == 1) {
             sb.append(" = ").append(ownAccountsIds.get(0));
